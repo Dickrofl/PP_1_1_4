2165eed4c091ade137f0430a015f3313d5aba34d
Dikrof <annanedavnyaya@yandex.ru>
2023-07-28 20:05:52 +0300

    second fixes

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,18 +1,32 @@
 package jm.task.core.jdbc;
 
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
-        userService.saveUser("Анна", "Каренина", (byte) 18);
-        userService.saveUser("Михаил", "Кочевний", (byte) 34);
-        userService.saveUser("София", "Овечкина", (byte) 29);
-        userService.saveUser("Виктор", "Шевченко", (byte) 14);
-        System.out.println(userService.getAllUsers());
+        User user1 = new User("Анна", "Каренина", (byte) 18);
+        userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        System.out.println("User с именем – " + user1.getName() + " добавлен в базу данных");
+        User user2 = new User("Михаил", "Кочевний", (byte) 34);
+        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        System.out.println("User с именем – " + user2.getName() + " добавлен в базу данных");
+        User user3 = new User("София", "Овечкина", (byte) 29);
+        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        System.out.println("User с именем – " + user3.getName() + " добавлен в базу данных");
+        User user4 = new User("Виктор", "Шевченко", (byte) 14);
+        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        System.out.println("User с именем – " + user4.getName() + " добавлен в базу данных");
+        List<User> users = userService.getAllUsers();
+        for (User user : users) {
+            System.out.println(user.toString());
+        }
         userService.cleanUsersTable();
         userService.dropUsersTable();
     }
