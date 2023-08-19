f682681772ad2225a6f3cad6a62653593bc9ffb3
Dikrof <annanedavnyaya@yandex.ru>
2023-08-19 12:48:45 +0300

    saveAndPrintUser

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class Util {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/new_schema";
+    private static final String URL = "jdbc:mysql://localhost:3306/jdbc";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "0703";
 
